6552a1ac635bc2abb3f069be9d9b6d1cbbb747ba
Antonín Karola <karolan1@fit.cvut.cz>
2021-02-09 20:29:16 +0100

    added user info to header/navbar

--- src/app/components/app-navbar/app-navbar.component.ts
+++ src/app/components/app-navbar/app-navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {InruptService} from "../../services/inrupt/inrupt.service";
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppNavbarComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  webId: string;
+  userLoggedIn;
 
-  ngOnInit(): void {
+  constructor(private readonly _inruptService: InruptService) {
   }
 
+  ngOnInit(): void {
+    this.userLoggedIn = this._inruptService.isLoggedIn();
+    this.onLogin();
+  }
+
+  onLogin() {
+    this._inruptService.session.addListener('login', () => {
+      this.userLoggedIn = true;
+      this.webId = this._inruptService.session.info.webId;
+      this._inruptService.getLoggedInUserName().then(username => {
+        this.username = username;
+      })
+    });
+  }
+
+  logout() {
+    this._inruptService.logout();
+    window.location.reload();
+  }
 }
