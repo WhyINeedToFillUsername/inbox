2661b32ba5a906e97ec9148976ba889a873cf0db
Antonín Karola <karolan1@fit.cvut.cz>
2021-02-15 11:17:04 +0100

    fix test

--- src/app/tabs/incoming/message-detail/message-detail.component.spec.ts
+++ src/app/tabs/incoming/message-detail/message-detail.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MessageDetailComponent } from './message-detail.component';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute} from "@angular/router";
+import {of} from "rxjs";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Overlay} from "@angular/cdk/overlay";
 
 describe('MessageDetailComponent', () => {
   let component: MessageDetailComponent;
   let fixture: ComponentFixture<MessageDetailComponent>;
 
+  const fakeQueryMap = {
+    params: of(['inboxUrl', 'messageId'])
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MessageDetailComponent ]
+      declarations: [ MessageDetailComponent ],
+      providers: [
+        MatSnackBar, Overlay, HttpClient,
+        {provide: ActivatedRoute, useValue: fakeQueryMap}
+      ]
     })
     .compileComponents();
   });
