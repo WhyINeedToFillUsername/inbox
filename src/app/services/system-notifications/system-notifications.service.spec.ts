e219acd6c090d3ebf1cf319f45974e0a2ef6f9de
Antonín Karola <karolan1@fit.cvut.cz>
2021-03-01 00:53:24 +0100

    fixed test

--- src/app/services/system-notifications/system-notifications.service.spec.ts
+++ src/app/services/system-notifications/system-notifications.service.spec.ts
@@ -2,12 +2,15 @@ import { TestBed } from '@angular/core/testing';
 
 import { SystemNotificationsService } from './system-notifications.service';
 import {RouterTestingModule} from "@angular/router/testing";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Overlay} from "@angular/cdk/overlay";
 
 describe('SystemNotificationsService', () => {
   let service: SystemNotificationsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      providers: [MatSnackBar, Overlay],
       imports: [RouterTestingModule]
     });
     service = TestBed.inject(SystemNotificationsService);
