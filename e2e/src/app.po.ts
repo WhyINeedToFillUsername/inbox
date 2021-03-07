3c47a2ec4cc71346fb2c657d5ca0dc2db2a71440
Antonín Karola <karolan1@fit.cvut.cz>
2021-03-07 13:04:46 +0100

    test readability

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -10,19 +10,19 @@ export class AppPage {
   }
 
   loginToInrupt() {
-    const EC = protractor.ExpectedConditions;
+    const until = protractor.ExpectedConditions;
 
     $('.login input').sendKeys("inrupt");
     $('.mat-option-text').click();
     $('#btnLogin').click();
     browser.waitForAngularEnabled(false);
-    browser.wait(EC.urlContains("inrupt.net/login"), 20000);
+    browser.wait(until.urlContains("inrupt.net/login"), 20000);
 
     $('#username').sendKeys("test-user1");
     $('#password').sendKeys("Inrupt@2021");
     $('#login').click();
     browser.waitForAngularEnabled(true);
 
-    browser.wait(EC.urlContains("/incoming"), 20000);
+    browser.wait(until.urlContains("/incoming"), 20000);
   }
 }
