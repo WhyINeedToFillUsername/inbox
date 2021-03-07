2f1939aa6f0b8c33cff9d62893d2ad1247c3e16a
Antonín Karola <karolan1@fit.cvut.cz>
2021-03-07 22:13:59 +0100

    run e2e in headless mode

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -13,7 +13,10 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    browserName: 'firefox'
+    browserName: 'firefox',
+    'moz:firefoxOptions': {
+      args: [ "--headless" ]
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
