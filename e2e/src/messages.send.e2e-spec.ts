99fda89bf91e20009af56010692f9ce0aa369b31
Antonín Karola <karolan1@fit.cvut.cz>
2021-03-08 22:58:53 +0100

    send simple message e2e test

--- e2e/src/messages.send.e2e-spec.ts
+++ e2e/src/messages.send.e2e-spec.ts
@@ -5,6 +5,7 @@ describe('send messages test', () => {
   let page: AppPage;
   const until = protractor.ExpectedConditions;
   const $recipientsInput = $('input');
+  const $submitButton = $('button[type=submit]');
 
   beforeEach(() => {
     page = new AppPage();
@@ -12,26 +13,44 @@ describe('send messages test', () => {
     page.loginToInrupt();
   });
 
-  it('form input validation + reading profile name', async () => {
+  it('form input validation + reading profile name', () => {
     element(by.linkText('Send message')).click();
     element(by.linkText('Simple message')).click();
 
     expect($('h1').getText()).toEqual('Send message');
 
-    const $submitButton = $('button[type=submit]');
     const $error = $('mat-error');
-    const $contact = $('mat-chip');
 
     $submitButton.click();
     expect($error.getText()).toEqual('No recipients!');
 
-
-    $recipientsInput.sendKeys('https://test-user1.inrupt.net/profile/card#me');
-    $recipientsInput.sendKeys(protractor.Key.ENTER);
-    browser.wait(until.visibilityOf($contact), 20000, 'Recipient is loaded.');
-    expect($contact.getText()).toContain('Test User 1: https://test-user1.inrupt.net/inbox/')
+    addRecipient('https://test-user1.inrupt.net/profile/card#me');
+    expect($('mat-chip').getText()).toContain('Test User 1: https://test-user1.inrupt.net/inbox/')
 
     $submitButton.click();
     expect($error.getText()).toEqual('Message cannot be empty!');
   });
+
+  it('send simple message', () => {
+    element(by.linkText('Send message')).click();
+    element(by.linkText('Simple message')).click();
+
+    expect($('h1').getText()).toEqual('Send message');
+
+    addRecipient('https://inbox1.inrupt.net/profile/card#me');
+
+    const $messageTextarea = $('textarea');
+    $messageTextarea.sendKeys('This is a message sent by an e2e test at ' + Date.now().toString());
+
+    $submitButton.click();
+
+    // snackbar is out of page root, can't use 'element(...)', but 'browser.driver.findElement(...)'
+    expect(browser.driver.findElement(by.tagName('snack-bar-container')).getText()).not.toContain('error');
+  });
+
+  function addRecipient(webId: string) {
+    $recipientsInput.sendKeys(webId);
+    $recipientsInput.sendKeys(protractor.Key.ENTER);
+    return browser.wait(until.visibilityOf($('mat-chip')), 20000, 'Recipient is loaded.');
+  }
 });
